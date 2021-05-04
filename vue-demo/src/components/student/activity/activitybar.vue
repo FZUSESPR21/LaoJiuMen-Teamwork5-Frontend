b756527a7844d3206b4c8455f12f21993c4e8c24
kwz <79499420+fzu-kwz@users.noreply.github.com>
2021-05-04 17:37:26 +0800

    课程活动侧边栏跳转

--- vue-demo/src/components/student/activity/activitybar.vue
+++ vue-demo/src/components/student/activity/activitybar.vue
@@ -1,34 +1,38 @@
 <template>
-    <el-row class="tac">
-  <el-col :span="12">
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose">
-      <el-menu-item index="1">
-        <i class="el-icon-location"></i>
-        <span slot="title">课程作业</span>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">课程小测</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <i class="el-icon-document"></i>
-        <span slot="title">已提交作业</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">通知列表</span>
-      </el-menu-item>
-    </el-menu>
-  </el-col>
-  
-</el-row>
+  <el-row class="tac">
+    <el-col :span="12">
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose">
 
+        <el-menu-item index="1">
+          <i class="el-icon-location"></i>
+<!--          <span slot="title">课程作业</span>-->
+          <router-link to="/student/activity/homeworklist"  tag="button" >课程作业</router-link>
+        </el-menu-item>
 
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">课程小测</span>
+        </el-menu-item>
+
+        <el-menu-item index="3">
+          <i class="el-icon-document"></i>
+<!--          <span slot="title">已提交作业</span>-->
+          <router-link to="/student/activity/submittedhomeworklist" tag="button" >已提交作业</router-link>
+        </el-menu-item>
+
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">通知列表</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
 </template>
+
 <script>
   export default {
     methods: {
@@ -37,6 +41,11 @@
       },
       handleClose(key, keyPath) {
         console.log(key, keyPath);
+      }
+    },
+    data() {
+      return {
+        userID: 'kwz'
       }
     }
   }
--- vue-demo/src/components/student/activity/activitymain.vue
+++ vue-demo/src/components/student/activity/activitymain.vue
@@ -1,51 +1,50 @@
 <template>
   <div>
-      
-    
-    
     <div id="head">课程活动
-        <p class="english">Activity</p>
+      <p class="english">Activity</p>
     </div>
+
     <div class="sidebar">
-    <activitybar></activitybar>
+      <activitybar></activitybar>
     </div>
+
     <div class="part">
-    
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
-import activitybar from '../activity/activitybar';
-export default {
-  name: "activitymain",
-  components: {
-    activitybar
+  import activitybar from "./activitybar";
+  export default {
+    name: "activitymain",
+    components: {
+      activitybar
+    }
   }
-}
 </script>
 
 <style scoped>
-#head{
-  font-size: 20px;
-  height: 100px;
-}
+  #head{
+    font-size: 20px;
+    height: 100px;
+  }
 
-.english{
-  color: rgb(179, 179, 179);
-  margin-top: 0;
-  font-size: 15px;
-}
+  .english{
+    color: rgb(179, 179, 179);
+    margin-top: 0;
+    font-size: 15px;
+  }
 
-.part{
-  margin-top: 0%;
-  background-color: rgb(228, 228, 228);
-  float:left;
-  
-}
+  .part{
+    margin-top: 0;
+    background-color: rgb(228, 228, 228);
+    float:left;
 
-.sidebar{
-    margin-top: 0%;
-    float: left;
-}
+  }
+
+  .sidebar{
+      margin-top: 0;
+      float: left;
+  }
 </style>
