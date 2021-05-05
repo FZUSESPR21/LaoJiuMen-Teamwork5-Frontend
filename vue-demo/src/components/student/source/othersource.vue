328737de2d1d2ac254878eeb6ebd9f4eb6a6b80b
kwz <79499420+fzu-kwz@users.noreply.github.com>
2021-05-06 00:18:50 +0800

    资源页面样式小修改

--- vue-demo/src/components/student/source/othersource.vue
+++ vue-demo/src/components/student/source/othersource.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <el-table :data="tableData" style="width: 100%" border>
+    <el-table :data="tableData"
+              style="width: 100%"
+              :header-cell-style="headeRowClass">
       <el-table-column
         v-for="(item,i) in tableCol"
         :key="i"
@@ -10,11 +12,17 @@
         align="center"
       ></el-table-column>
 
-      <el-table-column label="操作" v-if="showOper" align="center" width="300">
+      <el-table-column label="操作" v-if="showOper" align="center" width="200">
         <template slot-scope="scope">
           <!--          <router-link to="/student/activity/homeworkdetail" tag="button" >下载</router-link>-->
-          <el-button @click="lookClick">查看</el-button>
-          <el-button @click="downloadClick">下载</el-button>
+          <el-button @click="lookClick" class="button">查看</el-button>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作" v-if="showOper" align="center" width="200">
+        <template slot-scope="scope">
+          <!--          <router-link to="/student/activity/homeworkdetail" tag="button" >下载</router-link>-->
+          <el-button @click="downloadClick" class="button">下载</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -28,8 +36,8 @@ export default {
     return {
       showOper:true,
       tableCol: [
-        {prop: "name", label: "名称", width: 100},
-        {prop: "num", label: "下载量", width: 150},
+        {prop: "name", label: "名称", width: 200},
+        {prop: "num", label: "下载量", width: 200},
 
       ],
 
@@ -43,11 +51,20 @@ export default {
     };
   },
   methods: {
-
+    headeRowClass({row, column, rowIndex, columnIndex}){
+      //表头的背景颜色
+      if(rowIndex==0){
+        return 'background:#DCDCDC; ';
+      }
+    },
   },
 }
 </script>
 
 <style scoped>
-
+.button {
+  background-color: white;
+  color: dodgerblue;
+  border: 2px
+}
 </style>
