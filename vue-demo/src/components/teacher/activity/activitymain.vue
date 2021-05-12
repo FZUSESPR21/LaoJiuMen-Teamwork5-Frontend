016b040d6ea4681a72e45df8c0ce5a91ee882ec1
kwz <79499420+fzu-kwz@users.noreply.github.com>
2021-05-12 12:47:59 +0800

    更新

--- vue-demo/src/components/teacher/activity/activitymain.vue
+++ vue-demo/src/components/teacher/activity/activitymain.vue
@@ -15,36 +15,6 @@
     </div>
 
   </div>
-<!--  <div class="part">
-    <router-link to="/teacher/activity/noticelist" tag="button" >通知列表</router-link>
-    <router-link to="/teacher/activity/noticedetail" tag="button">通知内容</router-link>
-    <router-link to="/teacher/activity/noticedeliver" tag="button">发布通知</router-link>
-
-&lt;!&ndash;<el-row class="tac">
-  <el-col :span="12">
-    <el-menu
-      default-active="activeIndex"
-      class="el-menu-demo"
-      @open="handleOpen"
-      @close="handleClose">
-      <el-menu-item index="1">
-        <i class="el-icon-location"></i>
-        <span slot="title">作业列表</span>
-      </el-menu-item>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">发布作业</span>
-      </el-menu-item>
-      <el-menu-item index="3">
-        <i class="el-icon-document"></i>
-        <span slot="title">通知列表</span>
-      </el-menu-item>
-    </el-menu>
-  </el-col>
-  </el-row>
-  &ndash;&gt;
-  </div>-->
-<!--  <router-view id=content></router-view>-->
 
 
 </template>
